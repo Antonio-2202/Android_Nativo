ec9008f649255747bf90d6417f313762467438f1
OFICINA\antonio.martinez <antonio.martinez@onegolive.com>
2024-08-28 11:05:00 +0200

    Conseguir navegar de un fragment a otro y añadir animación de cuando se cambia el fragment

--- app/src/main/java/com/example/learning_andriod/ClientFragment.kt
+++ app/src/main/java/com/example/learning_andriod/ClientFragment.kt
@@ -4,11 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.learning_andriod.R.id.action_clientFragment_to_commercialDemandFragment
+import com.example.learning_andriod.R.id.btnNavigate
 import com.example.learning_andriod.R.layout.fragment_client
 
 class ClientFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(fragment_client, container, false)
+        val root = inflater.inflate(fragment_client, container, false)
+
+        val btnNavigate = root.findViewById<Button>(btnNavigate)
+
+        btnNavigate.setOnClickListener {
+            findNavController().navigate(action_clientFragment_to_commercialDemandFragment)
+        }
+
+        return root
     }
 }
