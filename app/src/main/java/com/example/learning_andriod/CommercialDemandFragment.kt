41d242de773b20d929e686c265c5a999d646c9fe
OFICINA\antonio.martinez <antonio.martinez@onegolive.com>
2024-08-29 08:32:41 +0200

    Cambiar la manera en donde asignamos las cosas, gracias a la explicaciond de Marcos y pasar parametro a otra pantalla

--- app/src/main/java/com/example/learning_andriod/CommercialDemandFragment.kt
+++ app/src/main/java/com/example/learning_andriod/CommercialDemandFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.learning_andriod.R.id.btnNavigate2
 import com.example.learning_andriod.R.id.tvUsername
 import com.example.learning_andriod.R.layout.fragment_commercial_demand
 
@@ -19,6 +22,17 @@ class CommercialDemandFragment : Fragment() {
         val username = args.username
         val tvUsername = view.findViewById<TextView>(tvUsername)
         tvUsername.text = username
+
+        val btnNavigate2 = view.findViewById<Button>(btnNavigate2)
+
+        btnNavigate2.setOnClickListener {
+            findNavController().navigate(
+                CommercialDemandFragmentDirections.actionCommercialDemandFragmentToVisitFragment(
+                    username = args.username.reversed()
+                )
+            )
+        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
--- app/src/main/java/com/example/learning_andriod/VisitFragment.kt
+++ app/src/main/java/com/example/learning_andriod/VisitFragment.kt
@@ -4,10 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.example.learning_andriod.R.id.tvUsernameReversed
 import com.example.learning_andriod.R.layout.fragment_visit
 
 class VisitFragment : Fragment() {
+
+    private val args: VisitFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val usernameReversed = args.username
+        val tvUsernameReversed = view.findViewById<TextView>(tvUsernameReversed)
+        tvUsernameReversed.text = usernameReversed
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(fragment_visit, container, false)
     }
