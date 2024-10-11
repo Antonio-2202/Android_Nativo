301a87ea2df0b2ea9a00e91fd7b650d8c47cb0fd
OFICINA\antonio.martinez <antonio.martinez@onegolive.com>
2024-10-11 10:09:57 +0200

    Añadir @SerializedName para poder parsearlo de un propio JSON con otro nombre

--- app/src/main/java/com/example/learning_andriod/domain/WsProduct.kt
+++ app/src/main/java/com/example/learning_andriod/domain/WsProduct.kt
@@ -1,10 +1,13 @@
 package com.example.learning_andriod.domain
 
+import com.google.gson.annotations.SerializedName
+
 data class WsProduct(
     val id: Int,
     val title: String,
     val price: Double,
     val description: String,
+    @SerializedName("image")
     val image: String
 ) {
     fun fromWsProduct(wsProduct: WsProduct): Product {
